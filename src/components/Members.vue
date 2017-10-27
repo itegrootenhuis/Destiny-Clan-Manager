bb9f705d65558bc08068cf8cb3f9a0fe72cbf9d4
BZS\itegrootenhuis <itegrootenhuis@bizstream.com>
2017-10-27 14:55:48 -0400

    Got characters activities, now need to figure out which activities are guided games and filter those out.

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="Members">
-  <p>Members: </p>
-    <ul>
-      <li v-for="Member in ClanMembers">{{Member.destinyUserInfo.displayName}} | {{Member.characterActivities}}</li>
-    </ul>
+    <div class="container" v-if="ClanMembers">
+      <div class="col-md-6" v-for="Member in ClanMembers"><h3 @click="toggleChild(Member.destinyUserInfo.displayName)">{{Member.destinyUserInfo.displayName}}</h3>
+        <div v-if="Member.characterActivities">
+          <div class="characters" v-for="(char, idx) in Member.characterActivities">
+            <p class="character">Character: {{idx}}</p>
+            <div v-for="(charActivity, i) in char.availableActivities">
+              <p v-if="(charActivity.Name === 'Leviathan' && charActivity.recommendedLight == 300)"><strong>Prestige-Raid:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+              <p v-if="(charActivity.Name === 'Leviathan' && charActivity.activityHash == 2693136601)"><strong>Raid test 1:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+              <p v-if="(charActivity.Name === 'Leviathan' && charActivity.activityHash == 287649202)"><strong>Raid test 2:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+              
+              <p v-else-if="($wildString.match('Nightfall*', charActivity.Name) && charActivity.recommendedLight == 300)"><strong>Prestige:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+              <p v-else-if="($wildString.match('Nightfall*', charActivity.Name) && charActivity.activityHash == 145302664)"><strong>Nightfall Test 1:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+              <p v-else-if="($wildString.match('Nightfall*', charActivity.Name) && charActivity.activityHash == 3920643231)"><strong>Nightfall Test 2:</strong> {{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+
+              <p v-else-if="charActivity.Name === 'Trials of the Nine'">{{charActivity.Name}}: {{charActivity.isCompleted}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,38 +31,64 @@ export default{
   created () {
     let $this = this
     // get Clan Members
-    $this.$Axios.get($this.$url + '/GroupV2/2219355/Members/?lc=en&fmt=true&currentPage=1&platformType=2')
-    .then(response => {
-      $this.ClanMembers = response.data.Response.results
-      $this.ClanMembers.forEach(function (member) {
-        $this.$Axios.get($this.$url + '/Destiny2/1/Profile/' + member.destinyUserInfo.membershipId + '/?components=204')
-        .then(response => {
-          member.characterActivities = response.data.Response.characterActivities.data
-          for (var char in member.characterActivities) {
-            member.characterActivities['' + char + ''].availableActivities.forEach(function (activity) {
-              $this.$Axios.get($this.$url + '/Destiny2/Manifest/DestinyActivityDefinition/' + activity.activityHash + '/')
-              .then(response => {
-                activity.Name = response.data.Response.displayProperties.name
-                // console.log(member)
-                // console.log($this.ClanMembers)
+    // console.log(!sessionStorage.getItem('ClanMembers'))
+    if (!sessionStorage.getItem('ClanMembers')) {
+      $this.$Axios.get($this.$url + '/GroupV2/2219355/Members/?lc=en&fmt=true&currentPage=1&platformType=2')
+      .then(response => {
+        $this.ClanMembers = response.data.Response.results
+        $this.ClanMembers.forEach(function (member) {
+          $this.$Axios.get($this.$url + '/Destiny2/1/Profile/' + member.destinyUserInfo.membershipId + '/?components=204')
+          .then(response => {
+            member.characterActivities = response.data.Response.characterActivities.data
+            for (var char in member.characterActivities) {
+              member.characterActivities[char].availableActivities.forEach(function (activity) {
+                $this.$Axios.get($this.$url + '/Destiny2/Manifest/DestinyActivityDefinition/' + activity.activityHash + '/')
+                .then(response => {
+                  activity.Name = response.data.Response.displayProperties.name
+                  // console.log(member)
+                  // console.log($this.ClanMembers)
+                  sessionStorage.setItem('ClanMembers', JSON.stringify($this.ClanMembers))
+                })
               })
-            })
-          }
-        })
-      }, this)
-    })
+            }
+          })
+        }, this)
+      })
+    } else {
+      // console.log('here')
+      $this.ClanMembers = JSON.parse(sessionStorage.getItem('ClanMembers'))
+      // console.log($this.ClanMembers)
+    }
+  },
+  mounted: function () {
+    $('.characters').toggle()
+  },
+  methods: {
+    toggleChild: function (clanMember) {
+      // let $this = this
+      let clickedMember = $('#Members').find('h3:contains(' + clanMember + ')')[0]
+      let membersCharacters = $(clickedMember).parent().find('.characters')
+      $(membersCharacters).toggle()
+    }
   }
 }
 </script>
 
 <!-- styling for the component -->
-<style scope>
-ul {
-  list-style-type: none;
-  padding: 0;
+<style scoped>
+#Members{
+ background-color: #777; 
 }
-
-li {
-  margin: 0 10px;
+p, h3 {
+  color: #fff;
+}
+h3{
+  cursor: pointer;
+  font-size: 3em;
+  text-decoration: underline
+}
+.character{
+  font-size: 1.3em;
 }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ import Home from './components/Home'
 import Members from './components/Members'
 import ClanSettigns from './components/ClanSettings'
 import AppSettings from './components/AppSettings'
+
 import Axios from 'axios'
+import wildstring from 'wildstring'
 
 Object.defineProperty(Vue.prototype, '$Axios', {value: Axios})
 Axios.defaults.headers.common['X-API-Key'] = '46b46fd8932d4fd2a67642ac21e1b3dc'
 Object.defineProperty(Vue.prototype, '$url', {value: 'https://www.bungie.net/platform'})
+
+Object.defineProperty(Vue.prototype, '$wildString', {value: wildstring})
 
 const routes = [
   { path: '/', component: Home },
